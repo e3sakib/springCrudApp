4b6d854f734c9a35bf4fff1c6c14d0a9af05870a
Sakib-Ahammed <satuhin6573@gmail.com>
2022-01-15 13:10:32 +0600

    first commit

--- src/main/java/MainSpringCrudApp.java
+++ src/main/java/MainSpringCrudApp.java
@@ -1,6 +1,7 @@
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class MainSpringCrudApp {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
